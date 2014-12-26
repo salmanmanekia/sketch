4a221e8df9fed1787255d09ae57571269951ea2e
muhammad.salman <salmanmanekia@gmail.com>
2014-12-27 01:49:40 +0200

    grids created

--- js/sketch.js
+++ js/sketch.js
@@ -1,14 +1,13 @@
 $(document).ready( function() {
-
 	createDivs(16);
-
 });
 
 var createDivs = function (grids) {
 	for (var i = 0; i < grids; i++) {
 		for (var j = 0; j < grids; j++) {
-			var grid = '<div id=' + i + '' + j + '></div>';
+			var grid = '<div id=' + i + '' + j + '>&nbsp;</div>';
 			$('#container').append(grid);
+			$('#container > div').addClass('grid-class');
 		}
 	}
 }
