a8ed771f6d49c305e38daf10e116ae8d6383c11e
muhammad.salman <salmanmanekia@gmail.com>
2015-01-15 23:42:21 +0200

    complete project

--- js/sketch.js
+++ js/sketch.js
@@ -1,20 +1,45 @@
 $(document).ready( function() {
 	createDivs(16);
+
+	$('#reset-btn').click(function(){
+		var grid_size = prompt('Please input the grid dimension', 16);
+		var node = document.getElementById('grids');
+		while (grids.firstChild) {
+			grids.removeChild(grids.firstChild);
+		}
+		createDivs(grid_size);
+	});
 });
 
+
 var createDivs = function (grids) {
+	var dimen = 25 * 16/grids;
+
 	for (var i = 0; i < grids; i++) {
 		for (var j = 0; j < grids; j++) {
-			var grid = '<div id=' + i + '' + j + '></div>';
-			var line = "same";
-			if (j == 0) {
-				line = "change"
-			}
-			console.log(line);
-			$('#container').append(grid);
+			var grid = $('<div class=grid id=' + i + '' + j + '></div>');
 
-			$('#container > div').addClass('grid-class').addClass(line);
+			$('#grids').append(grid);
+			if (j == 0) {
+				$('#grids > div:last').addClass('line-br');
+			} 
+			$('#grids > div:last').addClass('line');
+			$('#grids > div:last').css('width', dimen.toString());
+			$('#grids > div:last').css('height', dimen.toString());
 		}
-		
 	}
+	hoverFunc();
+}
+
+
+var hoverFunc = function() {
+	$('.grid').hover(
+		function() {
+			$(this).stop().animate({backgroundColor: "#000000"},"easeInBounce");
+		},
+		function() {
+			$(this).stop().animate({backgroundColor: "#D8D8D8"},"easeInElastic");
+		}
+	);
+
 }
